f0a6b4d1d0e8b7110f748dfe9be679456f40e202
Vladimir Pristansky <pristanskyvs@mail.ru>
2024-07-25 16:41:47 +0300

    работа с Generics

--- vehicle/models.py
+++ vehicle/models.py
@@ -11,3 +11,15 @@ class Car(models.Model):
     class Meta:
         verbose_name = 'автомобиль'
         verbose_name_plural = 'автомобили'
+
+
+class Moto(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'мотоцикл'
+        verbose_name_plural = 'мотоциклы'
