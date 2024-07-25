2e09bc20b08c8f5ba0478c73b21dde2265e147c3
Vladimir Pristansky <pristanskyvs@mail.ru>
2024-07-25 17:05:22 +0300

    Добавление зависимостей, "причесывание" урлов

--- vehicle/urls.py
+++ vehicle/urls.py
@@ -3,18 +3,24 @@ from django.urls import path
 from vehicle.apps import VehicleConfig
 from rest_framework.routers import DefaultRouter
 
-from vehicle.views import (CarViewSet, MotoCreateAPIView, MotoListAPIView, MotoRetrieveAPIView,
-                           MotoUpdateAPIView, MotoDestroyAPIView)
+from vehicle.views import (
+    CarViewSet,
+    MotoCreateAPIView,
+    MotoListAPIView,
+    MotoRetrieveAPIView,
+    MotoUpdateAPIView,
+    MotoDestroyAPIView,
+)
 
 app_name = VehicleConfig.name
 
 router = DefaultRouter()
-router.register(r'cars', CarViewSet, basename='cars')
+router.register(r"cars", CarViewSet, basename="cars")
 
 urlpatterns = [
-    path('moto/create/', MotoCreateAPIView.as_view(), name='moto-create'),
-    path('moto/', MotoListAPIView.as_view(), name='moto-list'),
-    path('moto/<int:pk>/', MotoRetrieveAPIView.as_view(), name='moto-get'),
-    path('moto/update/<int:pk>/', MotoUpdateAPIView.as_view(), name='moto-update'),
-    path('moto/delete/<int:pk>/', MotoDestroyAPIView.as_view(), name='moto-delete'),
+    path("moto/create/", MotoCreateAPIView.as_view(), name="moto-create"),
+    path("moto/", MotoListAPIView.as_view(), name="moto-list"),
+    path("moto/<int:pk>/", MotoRetrieveAPIView.as_view(), name="moto-get"),
+    path("moto/update/<int:pk>/", MotoUpdateAPIView.as_view(), name="moto-update"),
+    path("moto/delete/<int:pk>/", MotoDestroyAPIView.as_view(), name="moto-delete"),
 ] + router.urls
