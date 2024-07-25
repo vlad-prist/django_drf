27bfaececd49b4a145296bb596e79a17d5e88438
Vladimir Pristansky <pristanskyvs@mail.ru>
2024-07-25 13:36:51 +0300

    работа с ViewSet

--- config/settings.py
+++ config/settings.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'users',
+    'vehicle',
 
 ]
 
---
+++ vehicle/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+
+from vehicle.models import Car
+from vehicle.serializers import CarSerializer
+
+
+class CarViewSet(viewsets.ModelViewSet):
+    serializer_class = CarSerializer
+    queryset = Car.objects.all()
