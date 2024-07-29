281fe2b0938ad2614a48db05f99d9625cc69ad3e
Vladimir Pristansky <pristanskyvs@mail.ru>
2024-07-29 13:40:42 +0300

    Кастомизация сериализаторов у классов Car и Moto

--- vehicle/serializers.py
+++ vehicle/serializers.py
@@ -4,15 +4,27 @@ from vehicle.models import Car, Moto, Milage
 
 
 class CarSerializer(serializers.ModelSerializer):
+    last_milage = serializers.IntegerField(source='milage_set.all.first.milage')
+    #milage_set - походу это related_name
+    #first - порядковый номер объекта (потому что у нас год в Модели указан минус year)
+    #milage - поле, в котором хранится милаж
+
     class Meta:
         model = Car
         fields = '__all__'
 
 
 class MotoSerializer(serializers.ModelSerializer):
+    last_milage = serializers.SerializerMethodField()
+
     class Meta:
         model = Moto
         fields = '__all__'
+
+    def get_last_milage(self, obj):
+        if obj.milage_set.all().first():
+            return obj.milage_set.all().first().milage
+        return 0
 
 
 class MilageSerializer(serializers.ModelSerializer):
